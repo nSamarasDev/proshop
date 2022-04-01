342eda71ef3d0e63c3d634ebe2d1d223068997fe
Nichols Samaras <n.samaras4@outlook.com>
2022-04-01 16:05:21 -0400

    updated registerScreen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,9 @@ import {
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGOUT,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
   } from "../constants/userConstants";
    
   import axios from "axios";
@@ -48,3 +51,44 @@ import {
     localStorage.removeItem('userInfo')
     dispatch({ type: USER_LOGOUT })
   }
+
+  export const register = (name, email, password) => async (dispatch) => {
+    try {
+      dispatch({ type: USER_REGISTER_REQUEST });
+   
+      // Declare content type so request can be parsed as JSON
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+   
+      // Extract name,email and password from route
+      const { data } = await axios.post(
+        "/api/users",
+        { name, email, password },
+        config
+      );
+   
+      // If successful return the payload with email and password
+      dispatch({
+        type: USER_REGISTER_SUCCESS,
+        payload: data,
+      });
+
+      dispatch({
+        type: USER_LOGIN_SUCCESS,
+        payload: data,
+      })
+   
+      localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+      dispatch({
+        type: USER_REGISTER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
