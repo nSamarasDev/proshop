e247eb335ca974fad3b7bbe1cb62ad58cd3070cf
Nichols Samaras <n.samaras4@outlook.com>
2022-04-03 18:32:54 -0400

    profile screen added

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -6,6 +6,9 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
   } from "../constants/userConstants";
    
   import axios from "axios";
@@ -85,6 +88,40 @@ import {
     } catch (error) {
       dispatch({
         type: USER_REGISTER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+
+  export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: USER_DETAILS_REQUEST });
+
+      const { userLogin: { userInfo } } = getState()
+   
+      // Declare content type so request can be parsed as JSON
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`
+        },
+      };
+   
+      // Extract name,email and password from route
+      const { data } = await axios.get(`/api/users/${id}`,config);
+   
+      // If successful return the payload with email and password
+      dispatch({
+        type: USER_DETAILS_SUCCESS,
+        payload: data,
+      });
+
+    } catch (error) {
+      dispatch({
+        type: USER_DETAILS_FAIL,
         payload:
           error.response && error.response.data.message
             ? error.response.data.message
