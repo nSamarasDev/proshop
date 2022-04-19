3b075c08b15815143c993823397121c62f1b992d
Nichols Samaras <n.samaras4@outlook.com>
2022-04-18 23:01:15 -0400

    update

---
+++ frontend/src/actions/orderActions.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import {
+    ORDER_CREATE_REQUEST,
+    ORDER_CREATE_SUCCESS,
+    ORDER_CREATE_FAIL,
+} from '../constants/orderConstants'
+
+export const createOrder = (order) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: ORDER_CREATE_REQUEST, });
+
+      const { 
+    userLogin: { userInfo }, 
+    } = getState()
+   
+      // Declare content type so request can be parsed as JSON
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`
+        },
+      };
+   
+      // Extract cart contents and make request
+      const { data } = await axios.post(`/api/orders`, order, config);
+   
+      // If successful return the payload and pass it down to the state
+      dispatch({
+        type: ORDER_CREATE_SUCCESS,
+        payload: data,
+      });
+
+    } catch (error) {
+      dispatch({
+        type: ORDER_CREATE_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
