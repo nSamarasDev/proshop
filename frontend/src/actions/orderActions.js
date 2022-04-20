94801bd51b9fa631f01551e94a03cb3450ea2faa
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-20 10:53:14 -0400

    update

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -3,6 +3,9 @@ import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
     ORDER_CREATE_FAIL,
+    ORDER_DETAILS_FAIL,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_REQUEST,
 } from '../constants/orderConstants'
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -33,6 +36,41 @@ export const createOrder = (order) => async (dispatch, getState) => {
     } catch (error) {
       dispatch({
         type: ORDER_CREATE_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
+
+  export const getOrderDetails = (id) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: ORDER_DETAILS_REQUEST, });
+
+    const { 
+    userLogin: { userInfo }, 
+    } = getState()
+   
+      // Declare content type so request can be parsed as JSON
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`
+        },
+      };
+   
+      // Extract cart contents and make request
+      const { data } = await axios.get(`/api/orders${id}`, config);
+   
+      // If successful return the payload and pass it down to the state
+      dispatch({
+        type: ORDER_DETAILS_SUCCESS,
+        payload: data,
+      });
+
+    } catch (error) {
+      dispatch({
+        type: ORDER_DETAILS_FAIL,
         payload:
           error.response && error.response.data.message
             ? error.response.data.message
