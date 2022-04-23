05ba014f81f27543403b86ed6c433ff37d347b3c
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-22 21:45:39 -0400

    u[date

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -11,7 +11,8 @@ ORDER_PAY_SUCCESS,
 ORDER_PAY_FAIL,
 ORDER_LIST_MY_REQUEST,
 ORDER_LIST_MY_SUCCESS,
-ORDER_LIST_MY_FAIL, 
+ORDER_LIST_MY_FAIL,
+ORDER_LIST_MY_RESET, 
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -100,6 +101,8 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
                 loading: false,
                 error: action.payload,
             }
+        case ORDER_LIST_MY_RESET:
+            return { orders: [] }
     default:  
     return state
     }
