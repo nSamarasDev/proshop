c3893a79ee5f7ea1d3b429c345275fb3d01270e2
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-22 21:47:13 -0400

    update

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,6 +1,7 @@
 import { 
   USER_DETAILS_FAIL, 
   USER_DETAILS_REQUEST, 
+  USER_DETAILS_RESET, 
   USER_DETAILS_SUCCESS, 
   USER_LOGIN_FAIL, 
   USER_LOGIN_REQUEST, 
@@ -51,7 +52,9 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
       case USER_DETAILS_SUCCESS:
         return { loading: false, user: action.payload }
       case USER_DETAILS_FAIL:
-        return { loading: false, error: action.payload }   
+        return { loading: false, error: action.payload } 
+      case USER_DETAILS_RESET:  
+        return { user: {} }
       default:
         return state
     }
