19c905941cdc9b21da963e1ec7070f7d64b3deed
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-23 12:47:49 -0400

    update

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,210 +1,213 @@
 import {
-    USER_LOGIN_REQUEST,
-    USER_LOGIN_SUCCESS,
-    USER_LOGIN_FAIL,
-    USER_LOGOUT,
-    USER_REGISTER_REQUEST,
-    USER_REGISTER_SUCCESS,
-    USER_REGISTER_FAIL,
-    USER_DETAILS_REQUEST,
-    USER_DETAILS_SUCCESS,
-    USER_DETAILS_FAIL,
-    USER_UPDATE_PROFILE_SUCCESS,
-    USER_UPDATE_PROFILE_FAIL,
-    USER_UPDATE_PROFILE_REQUEST,
-    USER_DETAILS_RESET,
-    USER_LIST_REQUEST,
-    USER_LIST_SUCCESS,
-    USER_LIST_FAIL,
-  } from "../constants/userConstants";
-  import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
-   
-  import axios from "axios";
-   
-  // Login request to get token
-  export const login = (email, password) => async (dispatch) => {
-    try {
-      dispatch({ type: USER_LOGIN_REQUEST });
-   
-      // Declare content type so request can be parsed as JSON
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-   
-      // Extract email and password from route
-      const { data } = await axios.post(
-        "/api/users/login",
-        { email, password },
-        config
-      );
-   
-      // If successful return the payload with email and password
-      dispatch({
-        type: USER_LOGIN_SUCCESS,
-        payload: data,
-      });
-   
-      localStorage.setItem("userInfo", JSON.stringify(data));
-    } catch (error) {
-      dispatch({
-        type: USER_LOGIN_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_DETAILS_RESET,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
+  USER_LIST_RESET,
+} from '../constants/userConstants';
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 
-  export const logout = () => (dispatch) => {
-    localStorage.removeItem('userInfo')
-    dispatch({ type: USER_LOGOUT })
-    dispatch({ type: USER_DETAILS_RESET })
-    dispatch({ type: ORDER_LIST_MY_RESET })
+import axios from 'axios';
+
+// Login request to get token
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+
+    // Declare content type so request can be parsed as JSON
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    // Extract email and password from route
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    );
+
+    // If successful return the payload with email and password
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
+};
 
-  export const register = (name, email, password) => async (dispatch) => {
-    try {
-      dispatch({ type: USER_REGISTER_REQUEST });
-   
-      // Declare content type so request can be parsed as JSON
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-   
-      // Extract name,email and password from route
-      const { data } = await axios.post(
-        "/api/users",
-        { name, email, password },
-        config
-      );
-   
-      // If successful return the payload with email and password
-      dispatch({
-        type: USER_REGISTER_SUCCESS,
-        payload: data,
-      });
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
+  dispatch({ type: USER_LIST_RESET });
+};
 
-      dispatch({
-        type: USER_LOGIN_SUCCESS,
-        payload: data,
-      })
-   
-      localStorage.setItem("userInfo", JSON.stringify(data));
-    } catch (error) {
-      dispatch({
-        type: USER_REGISTER_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
 
-  export const getUserDetails = (id) => async (dispatch, getState) => {
-    try {
-      dispatch({ type: USER_DETAILS_REQUEST });
+    // Declare content type so request can be parsed as JSON
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
 
-      const { userLogin: { userInfo } } = getState()
-   
-      // Declare content type so request can be parsed as JSON
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${userInfo.token}`
-        },
-      };
-   
-      // Extract name,email and password from route
-      const { data } = await axios.get(`/api/users/${id}`,config);
-   
-      // If successful return the payload with email and password
-      dispatch({
-        type: USER_DETAILS_SUCCESS,
-        payload: data,
-      });
+    // Extract name,email and password from route
+    const { data } = await axios.post(
+      '/api/users',
+      { name, email, password },
+      config
+    );
 
-    } catch (error) {
-      dispatch({
-        type: USER_DETAILS_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+    // If successful return the payload with email and password
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
 
-  export const updateUserProfile = (user) => async (dispatch, getState) => {
-    try {
-      dispatch({ type: USER_UPDATE_PROFILE_REQUEST, });
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-      const { userLogin: { userInfo } } = getState()
-   
-      // Declare content type so request can be parsed as JSON
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${userInfo.token}`
-        },
-      };
-   
-      // Extract name,email and password from route
-      const { data } = await axios.put(`/api/users/profile`, user, config);
-   
-      // If successful return the payload with email and password
-      dispatch({
-        type: USER_UPDATE_PROFILE_SUCCESS,
-        payload: data,
-      });
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-    } catch (error) {
-      dispatch({
-        type: USER_UPDATE_PROFILE_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_DETAILS_REQUEST });
 
-  export const listUsers = () => async (dispatch, getState) => {
-    try {
-      dispatch({ type: USER_LIST_REQUEST, });
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-      const { userLogin: { userInfo } } = getState()
-   
-      // Declare content type so request can be parsed as JSON
-      const config = {
-        headers: {
-          Authorization: `Bearer ${userInfo.token}`
-        },
-      };
-   
-      // Extract name,email and password from route
-      const { data } = await axios.get(`/api/users`, config);
-   
-      // If successful return the payload with email and password
-      dispatch({
-        type: USER_LIST_SUCCESS,
-        payload: data,
-      });
+    // Declare content type so request can be parsed as JSON
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
-    } catch (error) {
-      dispatch({
-        type: USER_LIST_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+    // Extract name,email and password from route
+    const { data } = await axios.get(`/api/users/${id}`, config);
 
-  
+    // If successful return the payload with email and password
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_UPDATE_PROFILE_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    // Declare content type so request can be parsed as JSON
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    // Extract name,email and password from route
+    const { data } = await axios.put(`/api/users/profile`, user, config);
+
+    // If successful return the payload with email and password
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listUsers = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_LIST_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    // Declare content type so request can be parsed as JSON
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    // Extract name,email and password from route
+    const { data } = await axios.get(`/api/users`, config);
+
+    // If successful return the payload with email and password
+    dispatch({
+      type: USER_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
