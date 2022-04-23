f6d9c2513ddaefe00a3667088393b955a87f0512
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-23 09:48:01 -0400

    update

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -12,7 +12,7 @@ import {
   USER_REGISTER_SUCCESS, 
   USER_UPDATE_PROFILE_FAIL, 
   USER_UPDATE_PROFILE_REQUEST,
-  USER_UPDATE_PROFILE_SUCCESS
+  USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants"
 
 export const userLoginReducer = (state = {}, action) => {
