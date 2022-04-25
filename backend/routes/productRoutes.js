d2c320d7701a856503097becd39b628736dd8dda
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-25 15:47:08 -0400

    update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,13 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware';
 
-// @ desc   Fetch all products
-// @ route  Get /api/products
-// @ access   Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // res.status(401)
-    // throw new Error('Not Authorized')  // errorTesting
-    res.json(products);
-  })
-);
-
-// @ desc   Fetch single product
-// @ route  Get /api/products/:id
-// @ access   Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
