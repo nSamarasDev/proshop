dec0c71597d75f9217f872b5c842ffb0050a434f
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-25 14:35:30 -0400

    update will

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,33 +1,46 @@
 // import express from 'express'
 // const router = express.Router()
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
-
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
 
 // @dec      Fetch all products
 // @route    Get /api/products
 // @access   Public
-const getProducts = asyncHandler(async(req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-})
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
 
 // @ desc   Fetch single product
 // @ route  Get /api/products/:id
 // @ access   Public
-const getProductById = asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
-    if(product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-    res.json(product)
-})
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+  res.json(product);
+});
 
-export {
-    getProducts,
-    getProductById
-}
+// @ desc   Delete a product
+// @ route  DELETE /api/products/:id
+// @ access   Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  // If the product exists ? then
+  if (product) {
+    await proudct.remove();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+  res.json(product);
+});
+
+export { getProducts, getProductById, deleteProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,44 @@
-import express from 'express'
-import asyncHandler from 'express-async-handler'
-const router = express.Router()
-import Product from '../models/productModel.js'
+// import express from 'express';
+// const router = express.Router();
+// import {
+//   getProducts,
+//   getProductById,
+// } from '../controllers/productController.js';
 
+// export default router;
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+const router = express.Router();
+import Product from '../models/productModel.js';
 
 // @ desc   Fetch all products
 // @ route  Get /api/products
 // @ access   Public
-router.get('/', asyncHandler(async(req, res) => {
-    const products = await Product.find({})
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
     // res.status(401)
     // throw new Error('Not Authorized')  // errorTesting
-    res.json(products)
-}))
+    res.json(products);
+  })
+);
 
 // @ desc   Fetch single product
 // @ route  Get /api/products/:id
 // @ access   Public
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-    if(product) {
-        res.json(product)
+    if (product) {
+      res.json(product);
     } else {
-        res.status(404)
-        throw new Error('Product not found')
+      res.status(404);
+      throw new Error('Product not found');
     }
+  })
+);
 
-   
-}))
-
-export default router
+export default router;
