f3e85efba23eb3ca0341edb87eb6230d0650913b
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-26 11:32:14 -0400

    update

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,52 +1,87 @@
-import axios from 'axios'
-import { 
-    PRODUCT_DETAILS_FAIL,
-    PRODUCT_DETAILS_REQUEST,
-    PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_LIST_FAIL,
-    PRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_SUCCESS,
-} from '../constants/productConstants'
+import axios from 'axios';
+import {
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+} from '../constants/productConstants';
 
 export const listProducts = () => async (dispatch) => {
-    try {
-        dispatch({ type: PRODUCT_LIST_REQUEST })
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
 
-        const { data } = await axios.get('/api/products')
+    const { data } = await axios.get('/api/products');
 
-        dispatch({
-            type: PRODUCT_LIST_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: PRODUCT_LIST_FAIL,
-            payload: 
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.messsage
-        })
-    }
-}
-
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.messsage,
+    });
+  }
+};
 
 export const listProductDetails = (id) => async (dispatch) => {
-    try {
-        dispatch({ type: PRODUCT_DETAILS_REQUEST })
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-        const { data } = await axios.get(`/api/products/${id}`)
+    const { data } = await axios.get(`/api/products/${id}`);
 
-        dispatch({
-            type: PRODUCT_DETAILS_SUCCESS,
-            payload: data,
-        })
-    } catch (error) {
-        dispatch({
-            type: PRODUCT_DETAILS_FAIL,
-            payload: 
-                error.response && error.response.data.message
-                    ? error.response.data.message
-                    : error.messsage
-        })
-    }
-}
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.messsage,
+    });
+  }
+};
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_DELETE_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    // Declare content type so request can be parsed as JSON
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    // Extract cart contents and make request
+    await axios.get(`/api/products/${id}`, config);
+
+    // If successful return the payload and pass it down to the state
+    dispatch({
+      type: PRODUCT_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
