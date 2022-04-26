c9fc24b23683b434c8a859af0cbe5ce28961272c
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-26 10:04:45 -0400

    update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,11 +3,9 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
-  deleteProduct,
 } from '../controllers/productController.js';
-import { protect, admin } from '../middleware/authMiddleware';
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router.route('/:id').get(getProductById);
 
 export default router;
