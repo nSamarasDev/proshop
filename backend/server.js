2486acabfbe6c144235cf25f13b3f5cf132c6323
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-27 13:46:56 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
-import express  from 'express'
-import colors from 'colors'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
-import dotenv from 'dotenv'
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
+import path from 'path';
+import express from 'express';
+import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // Custom middleware access point
 // app.use((req, res, next) => {
@@ -22,26 +24,34 @@ app.use(express.json())
 // })
 
 app.get('/', (req, res) => {
-    res.send('API is running...Hi Nick...')
-})
+  res.send('API is running...Hi Nick...');
+});
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
-res.send(process.env.PAYPAL_CLIENT_ID)
-)
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Error middleware access point
 // This error responds json trace to Postman
-app.use(notFound)
+app.use(notFound);
 
 // Error middleware access point
 // Set status code before error
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
