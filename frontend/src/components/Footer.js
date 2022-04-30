69153170891109c87286407ec38434ab6f18bb20
Nicholas Samaras <n.samaras4@outlook.com>
2022-04-30 14:49:09 -0400

    update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
   return (
     <footer>
-        <Container>
-            <Row>
-                <Col className='text-center' py-3>Copyright &copy; ProShop</Col>
-                    
-                
-            </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; ProShop</Col>
+        </Row>
+      </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
