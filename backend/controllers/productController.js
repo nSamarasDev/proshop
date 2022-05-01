571819cbba49f8121e17f62d28fce5d3ddd6530b
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-01 13:25:20 -0400

    update

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,13 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
-// @ desc   Fetch all products
-// @ route  Get /api/products
-// @ access   Public
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 2;
+  const page = Number(req.query.pageNumber) || 1;
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -14,10 +17,12 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {};
 
-  const products = await Product.find({ ...keyword });
-  // res.status(401)
-  // throw new Error('Not Authorized')  // errorTesting
-  res.json(products);
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @ desc   Fetch single product
