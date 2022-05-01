ea0b9ead6c99e70e10fcf020144c074345150fd9
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-01 11:08:35 -0400

    update

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,32 +1,35 @@
-import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
 
 const SearchBox = ({ history }) => {
-  const [keyword, setKeyword] = useState('')
+  const [keyword, setKeyword] = useState('');
 
   const submitHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (keyword.trim()) {
-      history.push(`/search/${keyword}`)
+      history.push(`/search/${keyword}`);
     } else {
-      history.push('/')
+      history.push('/');
     }
-  }
+  };
 
   return (
-    <Form onSubmit={submitHandler} inline>
-      <Form.Control
-        type='text'
-        name='q'
-        onChange={(e) => setKeyword(e.target.value)}
-        placeholder='Search Products...'
-        className='mr-sm-2 ml-sm-5'
-      ></Form.Control>
-      <Button type='submit' variant='outline-success' className='p-2'>
-        Search
-      </Button>
-    </Form>
-  )
-}
+    <>
+      <Form onSubmit={submitHandler} className="d-flex">
+        <Form.Control
+          type="text"
+          name="q"
+          onChange={(e) => setKeyword(e.target.value)}
+          placeholder="Search Products..."
+          className="mr-sm-2 ml-sm-5"
+        ></Form.Control>
+        <Button type="subit" variant="outline-success" className="p-2">
+          Search
+        </Button>
+      </Form>
+    </>
+  );
+};
 
-export default SearchBox
+export default withRouter(SearchBox);
